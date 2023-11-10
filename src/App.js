b072f475aa445cfb726114c33ce3b27c1204889c
unknown <art.rib@hotmail.com>
2023-11-10 17:33:21 -0300

    parte do corpo modulaarizado criado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Cabecalho from './componentes/cabecalho';
 import Apresentacao from './componentes/apresentacao';
+import Form from './componentes/form';
 
 function App() {
   return (
     <>
     <Cabecalho />
     <Apresentacao />
+
+    <Form />
 
 
     
--- src/componentes/apresentacao.js
+++ src/componentes/apresentacao.js
@@ -1,6 +1,19 @@
+import img1 from "../assets/img1.png"
+import "./estilos/apresentacao.css"
+
 function Apresentacao(){
     return(
-        <></>
+        <div className="apresentacao">
+            <div className="texto">
+                <h1>Lorem ipsum dolor sit amet, consectetur </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <button>Button</button>
+            </div>
+            <div className="imgspace">
+                <img src={img1} />
+            </div>
+
+        </div>
         
     )
 }
