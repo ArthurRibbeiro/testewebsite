6b972dc5c984ba5483a4ee51cde7e1619e96746a
unknown <art.rib@hotmail.com>
2023-11-11 16:17:02 -0300

    carrocelServiços finalizado

--- src/componentes/carrocelservicos/carrocelservicos.js
+++ src/componentes/carrocelservicos/carrocelservicos.js
@@ -1,21 +1,45 @@
 import '../estilos/carrocelservicos/carrocelservicos.css'
 import Celulacarrocelservicos from './celulacarrocelservicos'
 
+import "../estilos/carrocelservicos/carrocelservicos.css"
+
+
+
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 function Carrocelservicos(){
+    const settings = {
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed:500,
+        slidesToShow:4,
+        slidesToScroll:3,
+    }
+    
     return(
-        <div>
+        <div className='carrocelServicosSpace'>
             <h2>Serviços</h2>
-            <div className='galeria'>
+
+            <Slider style={{ width: "90%", margin: "0 auto" }} {...settings}>
+                <Celulacarrocelservicos />
+                <Celulacarrocelservicos />
+                <Celulacarrocelservicos />
+                <Celulacarrocelservicos />
                 <Celulacarrocelservicos />
                 <Celulacarrocelservicos />
                 <Celulacarrocelservicos />
                 <Celulacarrocelservicos />
                 
 
-            </div>
+            
+            </Slider>
         </div>
 
     )
 }
 
 export default Carrocelservicos
+
